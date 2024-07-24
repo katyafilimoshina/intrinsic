46c0806bccbb15122c8a09db1b707068f9b13da2
katyafilimoshina <filimoshinaek@gmail.com>
2024-07-24 10:46:27 +0300

    üpdated documentation

--- functional/magnitude.py
+++ functional/magnitude.py
@@ -6,12 +6,15 @@ def magnitude(X: np.ndarray, distances: bool = False):
     """
     Computes the magnitude of a distance matrix or the matrix itself.
 
-    Parameters:
-    X (np.ndarray): Input matrix. If `distances` is False, X is assumed to be a point cloud and distances are computed.
-    distances (bool): Whether X is already a distance matrix. If False, distances will be computed.
+    Args:
+        X (np.ndarray): 
+            Input matrix. If `distances` is False, X is assumed to be a point cloud and distances are computed.
+        distances (bool): 
+            Whether X is already a distance matrix. If False, distances will be computed.
 
     Returns:
-    float: The magnitude, which is the sum of the inverse of the exponentiated distance matrix.
+        float: 
+            The magnitude, which is the sum of the inverse of the exponentiated distance matrix.
     """
     d = X if distances else distance_matrix(X, X)
     d = np.exp(-d)
